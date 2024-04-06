b3e1017dd18f9832ea8e0895cea5bf12373e1a12
17:17 <eexet17@gmail.com>
2024-04-06 16:42:26 +0300

    Updated header,footer

---
+++ Steam-Project/src/app/editions/editions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editions',
+  standalone: true,
+  imports: [],
+  templateUrl: './editions.component.html',
+  styleUrl: './editions.component.css'
+})
+export class EditionsComponent {
+
+}
--- Steam-Project/src/app/header/header.component.ts
+++ Steam-Project/src/app/header/header.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FloatLabelType, MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule, MatButton } from '@angular/material/button';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  imports: [MatToolbarModule,
+            MatButtonModule,
+            MatIconModule,
+            MatFormFieldModule,
+            MatInputModule,
+            MatSelectModule,
+            MatButton,
+            MatButtonModule,
+            MatMenuModule, 
+            FormsModule,
+            MatButtonToggleModule,
+            ReactiveFormsModule
+          ],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  
 }
